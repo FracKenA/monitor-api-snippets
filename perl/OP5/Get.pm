ba2e0b1a686083c6b36c9135b73724f94d3ac078
Ryan Quinn <rquinn@op5.com>
2018-07-10 21:02:16 -0500

    Adding perl files.
     On branch master
     Changes to be committed:
    	new file:   perl/OP5/Get.pm
    	new file:   perl/op5driver.pl

---
+++ perl/OP5/Get.pm
@@ -0,0 +1,25 @@
+#
+#===============================================================================
+#
+#         FILE: OP5lib.pm
+#
+#  DESCRIPTION: Library of OP5 API examples
+#
+#        FILES: ---
+#         BUGS: ---
+#        NOTES: ---
+#      VERSION: 0.1
+#       AUTHOR: Ryan Quinn (RQ), rquinn@op5.com
+# ORGANIZATION: OP5, Inc.
+#      LICENSE: BSD License, Original 4-Clause License
+#      CREATED: 07/10/2018 08:53:06 PM
+#===============================================================================
+
+
+package OP5lib;
+
+use strict;
+use warnings 'all';
+
+
+1;
---
+++ perl/op5driver.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+#===============================================================================
+#
+#         FILE: op5driver.pl
+#
+#        USAGE: ./op5driver.pl
+#
+#  DESCRIPTION: Driver for Perl examples.
+#
+#    ARGUMENTS: ---
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#      VERSION: 0.1
+#       AUTHOR: Ryan Quinn (RQ), rquinn@op5.com
+# ORGANIZATION: OP5, Inc.
+#      LICENSE: BSD License, Original 4-Clause License
+#      CREATED: 07/10/2018 08:38:12 PM
+#===============================================================================
+
+use strict;
+use warnings 'all';
+use utf8;
+use 5.010;
+
+use lib '.';
+
+require OP5::Get;
+
+
